e2d3803f5f549b748d93d4520c7cb867e4b8cd5d
davidbyrne98 <davidbyrne684@gmail.com>
2022-01-11 13:24:18 +0000

    Adding some simple / tesing commands

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import os
 import hikari
 import lightbulb
+import aiohttp
 from dotenv import load_dotenv
 from Liff import GUILD_ID
 
@@ -13,13 +14,26 @@ bot = lightbulb.BotApp(
     intents=hikari.Intents.ALL,
     default_enabled_guilds=GUILD_ID,
 )
+
 bot.load_extensions_from("Liff/extensions/", must_exist=True)
+
 
 @bot.command
 @lightbulb.command("ping", description="The bot's ping")
 @lightbulb.implements(lightbulb.PrefixCommand)
 async def ping(ctx: lightbulb.Context) -> None:
     await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
+
+
+@bot.listen()
+async def on_starting(event: hikari.StartingEvent) -> None:
+    bot.d.aio_session = aiohttp.ClientSession()
+
+
+@bot.listen()
+async def on_stopping(event: hikari.StoppingEvent) -> None:
+    await bot.d.aio_session.close()
+
 
 if __name__ == "__main__":
     if os.name != "nt":
--- extensions/commands.py
+++ extensions/commands.py
@@ -1,8 +1,41 @@
 import hikari
 import lightbulb
 
-info_plugin = lightbulb.Plugin("Commands")
+commands_plugin = lightbulb.Plugin("Commands")
+
+@commands_plugin.command
+@lightbulb.command("commands", "IDK what to put here")
+@lightbulb.implements(lightbulb.SlashCommandGroup, lightbulb.PrefixCommandGroup)
+async def commands_group(ctx: lightbulb.Context) -> None:
+    pass  # as slash commands cannot have their top-level command ran, we simply pass here
+
+
+@commands_group.child
+@lightbulb.command("meme", "Fetch a meme")
+@lightbulb.implements(lightbulb.SlashSubCommand, lightbulb.PrefixSubCommand)
+async def meme_subcommand(ctx: lightbulb.Context) -> None:
+    async with ctx.bot.d.aio_session.get(
+        "https://meme-api.herokuapp.com/gimme"
+    ) as response:
+        res = await response.json()
+        if response.ok and res["nsfw"] != True:
+            link = res["postLink"]
+            title = res["title"]
+            img_url = res["url"]
+            embed = hikari.Embed(colour=0x3B9DFF)
+            embed.set_author(name=title, url=link)
+            embed.set_image(img_url)
+            await ctx.respond(embed)
+        else:
+            await ctx.respond(
+                "Could not fetch a meme :c", flags=hikari.MessageFlag.EPHEMERAL
+            )
+
+@commands_plugin.command
+@lightbulb.command("idk", "Yep Front")
+@lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
+async def idk_wtf(ctx: lightbulb.Context) -> None:
+    await ctx.respond("yep front")
 
 def load(bot: lightbulb.BotApp) -> None:
-    bot.add_plugin(info_plugin)
-
+    bot.add_plugin(commands_plugin)
